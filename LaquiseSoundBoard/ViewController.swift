d790bd7105bf8fb7abfcd04578ddeec992726f94
MiluskaLaquise <miluska.laquise@tecsup.edu.pe>
2023-05-24 08:43:25 -0500

    Initial Commit

---
+++ LaquiseSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaquiseSoundBoard
+//
+//  Created by MAC32 on 24/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
